2cb3ff25d7bd0e64aab10acc6c49f2546eec344c
Ege Uysal <hello@egeuysal.com>
2025-03-23 23:22:06 -0500

    Update layout.tsx

--- links/app/layout.tsx
+++ links/app/layout.tsx
@@ -184,8 +184,8 @@ export default async function RootLayout({
       </head>
       <body className={lato.className}>
         <LayoutWrapper>
-          <div className="min-h-screen flex flex-col items-center justify-between mb-12">
-            <main className="w-[85vw] max-w-7xl flex-1 flex flex-col py-4">
+          <div className="min-h-screen w-screen flex flex-col items-center justify-between mb-12">
+            <main className="w-[85vw] flex-1 flex flex-col py-4">
               <Header />
               <div className="flex-1 my-4">{children}</div>
               <Footer />
