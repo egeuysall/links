4517c0d9b0cd990a59688cec2e53ee6d4601e20f
Ege Uysal <hello@egeuysal.com>
2025-03-29 01:39:44 +0000

    Switch to pnpm

--- links/next.config.ts
+++ links/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  swcMinify: true,
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60,
+  },
+  typescript: {
+    ignoreBuildErrors: process.env.CI === "true",
+  },
 };
 
 export default nextConfig;
