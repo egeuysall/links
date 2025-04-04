908cff92d586a71bb5e40fa75c091c075f99dd42
Ege Uysal <hello@egeuysal.com>
2025-04-03 22:22:59 -0500

    Update PreviewPanel.tsx

--- links/app/components/links/PreviewPanel.tsx
+++ links/app/components/links/PreviewPanel.tsx
@@ -8,7 +8,7 @@ interface PreviewPanelProps {
 
 export const PreviewPanel: React.FC<PreviewPanelProps> = ({ html }) => {
   return (
-    <div className="lg:col-span-2 row-span-1 rounded-md text-[#593116]">
+    <div className="lg:col-span-2 row-span-1 rounded-lg text-[#593116]">
       <h3 className="text-3xl mb-4 font-bold text-center">Preview</h3>
       <iframe
         srcDoc={html}
